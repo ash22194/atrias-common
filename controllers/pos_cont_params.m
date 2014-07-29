d92091984fb89a6d47af427bc9b827923971f90d
jrvanwhy <jrvanwhy@gmail.com>
2014-07-29 16:27:13 -0400

    Adding in a sample controller. This includes the model reference block necessary to use the atrias system.

---
+++ controllers/pos_cont_params.m
@@ -0,0 +1,11 @@
+% This contains parameters for the position controller
+
+% Hip and leg saturation parameters
+hip_sat = 50;
+leg_sat = 50;
+
+% Gains
+leg_p = 50;
+leg_d = 5;
+hip_p = 50;
+hip_d = 5;
