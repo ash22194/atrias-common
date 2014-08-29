2a8154848f87ca2460ca591e259b7f84a39ab931
jrvanwhy <jrvanwhy@gmail.com>
2014-08-29 19:00:55 -0400

    Lots of testing and bug fixes.

--- controllers/pos_cont_params.m
+++ controllers/pos_cont_params.m
@@ -2,13 +2,16 @@
 
 % Hip and leg saturation parameters
 hip_sat = 50;
-leg_sat = 50;
+leg_sat = 60;
 
 % Gains
-leg_p = 500;
-leg_d = 40;
+leg_p = 5000;
+leg_d = 160;
 hip_p = 1500;
 hip_d = 10;
+
+% Targets
+splay_ang = 40;
 
 % Safety control
 sat_estops = false;
