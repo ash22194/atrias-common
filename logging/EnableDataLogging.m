70b306ec951348d828977c4bd08e7728ac5a482d
williamcmartin <wmartin@cmu.edu>
2016-10-18 19:01:25 -0400

    Updated controllers to run in concurrent execution with mixed rate systems.

---
+++ logging/EnableDataLogging.m
@@ -0,0 +1,114 @@
+function [  ] = EnableDataLogging( )
+    % Definitions
+    tg = slrt;
+    system_prefix = 'atrias_system/';
+    behavior_prefix = 'atrias_control/behavior_control/';
+    actuator_prefix = 'atrias_control/actuator_control/';
+    % Remove old file scopes
+    scopes = tg.getscope();
+    if ~isempty(scopes) 
+        scopes.stop;
+        for i=1:size(scopes,1)
+            if strcmp(scopes(i).type, 'File')
+                tg.remscope(i);
+            end
+        end
+    end
+    % Create scopes
+    number_of_fscopes = 2;
+    fscopes = [];
+    for i=1:number_of_fscopes
+        fscopes = [fscopes tg.addscope('file')];
+    end
+    set(fscopes(1:number_of_fscopes), ...
+        {'WriteMode', 'AutoRestart', 'DynamicFileName', 'WriteSize'}, ...
+        {'Lazy',      'on',          'on',              8192});
+    fscopes(1).Filename = 'F:\LOGS\LOG1_<%%%>.dat';
+    fscopes(1).Decimation = 4;
+    fscopes(1).NumSamples = 1000;
+    fscopes(2).Filename = 'F:\LOGS\LOG4_<%%%>.dat';
+    fscopes(2).Decimation = 1;
+    fscopes(2).NumSamples = 1000;
+    % Add signals to scopes
+    % 1khz scope
+    fscopes(1).addsignal(tg.getsignalid([actuator_prefix 'p1/s1'])); % commanded lateral torques
+    fscopes(1).addsignal(tg.getsignalid([actuator_prefix 'p1/s2'])); %
+    
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p6/s1'])); % accel x 
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p6/s2'])); % accel y
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p6/s3'])); % accel z
+    
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p4/s1'])); % right back leg
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p4/s2'])); % right back gear
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p4/s3'])); % right front leg
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p4/s4'])); % right front gear
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p4/s5'])); % left back leg
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p4/s6'])); % left back gear
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p4/s7'])); % left front leg
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p4/s8'])); % left front gear
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p4/s9'])); % right lateral
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p4/s10'])); % left lateral
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p4/s11'])); % roll 
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p4/s12'])); % yaw 
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p4/s13'])); % pitch 
+    
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p5/s1'])); % right back leg
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p5/s2'])); % right back gear
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p5/s3'])); % right front leg
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p5/s4'])); % right front gear
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p5/s5'])); % left back leg
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p5/s6'])); % left back gear
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p5/s7'])); % left front leg
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p5/s8'])); % left front gear
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p5/s9'])); % right lateral
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p5/s10'])); % left lateral
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p5/s11'])); % roll 
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p5/s12'])); % yaw 
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p5/s13'])); % pitch 
+    
+    fscopes(1).addsignal(tg.getsignalid([behavior_prefix 'p1/s1'])); % desired SEA torques
+    fscopes(1).addsignal(tg.getsignalid([behavior_prefix 'p1/s2'])); %
+    fscopes(1).addsignal(tg.getsignalid([behavior_prefix 'p1/s3'])); %
+    fscopes(1).addsignal(tg.getsignalid([behavior_prefix 'p1/s4'])); %
+    
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p14/s1'])); % elmo error codes
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p14/s2'])); %
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p14/s3'])); %
+    fscopes(1).addsignal(tg.getsignalid([system_prefix 'p14/s4'])); %
+    
+    % 4khz scope
+    fscopes(2).addsignal(tg.getsignalid([actuator_prefix 'p2/s1'])); % elmo commanded velocity
+    fscopes(2).addsignal(tg.getsignalid([actuator_prefix 'p2/s2'])); %
+    fscopes(2).addsignal(tg.getsignalid([actuator_prefix 'p2/s3'])); %
+    fscopes(2).addsignal(tg.getsignalid([actuator_prefix 'p2/s4'])); %
+    
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p7/s1'])); % elmo measured position
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p7/s2'])); % 
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p7/s3'])); % 
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p7/s4'])); % 
+    
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p8/s1'])); % elmo measured velocity
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p8/s2'])); % 
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p8/s3'])); % 
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p8/s4'])); % 
+    
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p9/s1'])); % elmo measured torque
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p9/s2'])); % 
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p9/s3'])); % 
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p9/s4'])); % 
+    
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p10/s1'])); % elmo demanded velocity
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p10/s2'])); % 
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p10/s3'])); % 
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p10/s4'])); % 
+    
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p11/s1'])); % elmo demanded torque
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p11/s2'])); % 
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p11/s3'])); % 
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p11/s4'])); % 
+    
+    fscopes(2).addsignal(tg.getsignalid([system_prefix 'p3/s1'])); % motors enabled
+
+    % Begin logging data
+    fscopes.start();
+end
--- scripts/actuator_control_params.m
+++ scripts/actuator_control_params.m
@@ -8,10 +8,10 @@ B1_lpf_accel = -2*exp(-lpf_damping*fcut_acceleration*sample_time)*cos(fcut_accel
 B2_lpf_accel = exp(-2*lpf_damping*fcut_acceleration*sample_time);
 A_lpf_accel = 1 + B1_lpf_accel + B2_lpf_accel;
 %-- Low Pass filter for torque derivative
-fcut_dtau = 60*(2*pi); % Hz, Low pass filter cutoff frequency when calculating acceleration from velocity
+fcut_dtau = 100*(2*pi); % Hz, Low pass filter cutoff frequency when calculating acceleration from velocity
 lpf_damping = sqrt(2)/2; % butterworth damping ratio
-B1_lpf_dtau = -2*exp(-lpf_damping*fcut_dtau*sample_time)*cos(fcut_dtau*sample_time*sqrt(1-lpf_damping^2));
-B2_lpf_dtau = exp(-2*lpf_damping*fcut_dtau*sample_time);
+B1_lpf_dtau = -2*exp(-lpf_damping*fcut_dtau*elmo_sample_time)*cos(fcut_dtau*elmo_sample_time*sqrt(1-lpf_damping^2));
+B2_lpf_dtau = exp(-2*lpf_damping*fcut_dtau*elmo_sample_time);
 A_lpf_dtau = 1 + B1_lpf_dtau + B2_lpf_dtau;
 %-- Low Pass filter to reduce resonance
 fcut_res = 20*(2*pi); % Hz, Low pass filter cutoff frequency when calculating acceleration from velocity
